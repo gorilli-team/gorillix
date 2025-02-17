7f0954ac77e81681bc845d432134b7524e0c82ac
SamuelePassiatore <samuw.978@gmail.com>
2025-02-17 15:13:14 +0100

    import font awesome

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link 
+              rel="stylesheet" 
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" 
+              integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" 
+              crossOrigin="anonymous" 
+        />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
