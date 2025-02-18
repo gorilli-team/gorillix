3b22de6b80789a2b78ef2d5b1fb27367b4193aaf
SamuelePassiatore <samuw.978@gmail.com>
2025-02-18 15:42:39 +0100

    customize tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,6 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray: {
+          50: '#F4F2FC',
+          100: '#E4DFF6',
+          200: '#D1CAF2',
+          300: '#ACA0DD',
+          400: '#8A80B2',
+          500: '#342C53',
+          600: '#262041',
+          700: '#16112E',
+          800: '#04071F',
+          900: '#010213',
+        }
       },
     },
   },
