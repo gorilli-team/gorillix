3c3b64fa26500c073350cec6cccadbe535840fee
SamuelePassiatore <samuw.978@gmail.com>
2025-02-19 17:04:09 +0100

    update next config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  reactStrictMode: true,
   devIndicators: {
     appIsrStatus: false
-  },
+  }
 };
 
 export default nextConfig;
