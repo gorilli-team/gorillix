1b5c701f08500e31bcfa480eaf7e02edcf2693b8
SamuelePassiatore <samuw.978@gmail.com>
2025-02-28 16:31:05 +0100

    update contract addresses

--- frontend/src/app/utils/addresses.ts
+++ frontend/src/app/utils/addresses.ts
@@ -7,9 +7,9 @@ export interface ContractAddresses {
   }
   
   export const CONTRACT_ADDRESSES: ContractAddresses = {
-    TOKEN_A: '0x126230277775c3a25cd7a1f9dcada35d0278cb0c' as `0x${string}`,
-    TOKEN_B: '0xf944f935705be4572e8d29c342dc728ba8a78baf' as `0x${string}`,
-    GORILLIX: '0x2de70d920c3b43c3c32ab97fc0850b1bd2ff5a3d' as `0x${string}`,
-    FAUCET: '0xda92650877e8ed0656ac6de6f4c69a3680ba831f' as `0x${string}`,
-    ESCROW: '0xe01f8af91fec9a7b1a933c057daced7841efe2fc' as `0x${string}`
+    TOKEN_A: '0x9455bd9d1dd4a7e66e34ffc2e5de83caa92092ae' as `0x${string}`,
+    TOKEN_B: '0x1d96f7338ae4edfc835bd3d19297666c1e80b170' as `0x${string}`,
+    GORILLIX: '0x80e93b9420746ac25c420c3a7cc29b46bec34d26' as `0x${string}`,
+    FAUCET: '0xce02e799a74ae683fceceaada62497e82ed7cb4a' as `0x${string}`,
+    ESCROW: '0xcd8aca3fba1192607a87fec234fd13ed439ff36c' as `0x${string}`
   };
